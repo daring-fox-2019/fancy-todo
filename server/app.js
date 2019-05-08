e60bae70c231c8e51df2d25f53945793cec22548
qoyyima22 <45622790+qoyyima22@users.noreply.github.com>
2019-05-08 11:38:38 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const PORT = process.env.PORT || 3000
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env,DB, {useNewUrlParser: true})
+mongoose.connect(process.env.DB, {useNewUrlParser: true})
 const db = mongoose.connection
 
 db.on('error', console.error.bind(console, 'connection error:'))
