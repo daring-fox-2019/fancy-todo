3527cffb6080bdf6dafabdca7f8eca030cbb29d4
Michael <msaputra93@yahoo.com>
2019-05-24 12:55:37 +0700

    persiapan deploy

--- server/app.js
+++ server/app.js
@@ -1,10 +1,29 @@
 require('dotenv').config();
+
+
 const express = require('express');
 const app = express();
 
 const router = require('./routes')
 const mongoose = require('mongoose');
+// mongoose.connect(process.env.ATLAS_URL || 'mongodb://localhost:27017/fancy-todo-1', {useNewUrlParser: true})
 mongoose.connect(process.env.ATLAS_URL || 'mongodb://localhost:27017/fancy-todo-1', {useNewUrlParser: true})
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', function () {  
+console.log('Mongoose default connection open on' + process.env.ATLAS_URL);
+}); 
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {  
+console.log('Mongoose default connection error: ' + err);
+}); 
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {  
+console.log('Mongoose default connection disconnected'); 
+});
 
 const cors = require('cors')
 
