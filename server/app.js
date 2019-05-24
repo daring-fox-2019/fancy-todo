a5adce1a4773fc6cea0741b22c5399e6e6616320
rizky <andijani21.rizky@gmail.com>
2019-05-24 14:22:14 +0700

    update dotenv2

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 const cors = require('cors')
-const port = 3000
+const port = process.env.port
 const route = require('./routes/index')
 
 app.use(express.urlencoded({extended:false}))
